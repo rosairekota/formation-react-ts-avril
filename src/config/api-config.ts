576f6a9f49c068be62f6e2c83f038281dbb764dc
Christian Lisangola <jochri3@gmail.com>
2021-04-28 08:31:03 +0100

    mise en place de sass

--- src/config/api-config.ts
+++ src/config/api-config.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://contacts-webservice.herokuapp.com/api/contacts",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import store from "./state/store";
 import { Provider } from "react-redux";
+import "./assets/sass/main.scss";
 
 ReactDOM.render(
   <Provider store={store}>
